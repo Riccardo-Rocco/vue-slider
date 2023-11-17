e33ba0df0f4988afb139b0dc357c1c58e4f55d6f
Riccardo <riccardorocco2001@gmail.com>
2023-11-17 10:38:18 +0100

    correzione

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentSlide: 0,
+      slides: [
+        {
+          immagine: 'https://img.freepik.com/free-photo/close-up-friendly-looking-happy-positive-lucky-young-female-wearing-checked-shirt-t-shirt-smiling-broadly-delighted-feeling-awesome-joyful-day-enjoying-life-standing-white-background_176420-34722.jpg',
+          titolo: 'Esperienza fantastica!',
+          contenuto: 'Ho apprezzato molto il servizio e la qualità del prodotto. Sicuramente tornerò!',
+          autoreNome: 'Marco',
+          autoreCognome: 'Rossi'
+        },
+        {
+          immagine: 'https://img.freepik.com/free-photo/portrait-young-handsome-man-jean-shirt-smiling-with-crossed-arms_176420-12083.jpg',
+          titolo: 'Prodotto eccezionale!',
+          contenuto: 'Il prodotto supera le aspettative, funziona benissimo e ha un ottimo design.',
+          autoreNome: 'Laura',
+          autoreCognome: 'Bianchi'
+        },
+        {
+          immagine: 'https://img.freepik.com/free-photo/mand-holding-cup_1258-340.jpg',
+          titolo: 'Servizio impeccabile!',
+          contenuto: 'Il personale è cordiale e attento alle esigenze del cliente. Consigliato!',
+          autoreNome: 'Giovanni',
+          autoreCognome: 'Verdi'
+        }
+      ],
+    };
+  },
+  methods: {
+    prevSlide() {
+      this.currentSlide = (this.currentSlide - 1 + this.slides.length) % this.slides.length;
+    },
+    nextSlide() {
+      this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+    },
+  },
+});
 
-    const app = Vue.createApp({
-      data() {
-        return {
-          slides: [
-            {
-              immagine: 'https://img.freepik.com/free-photo/close-up-friendly-looking-happy-positive-lucky-young-female-wearing-checked-shirt-t-shirt-smiling-broadly-delighted-feeling-awesome-joyful-day-enjoying-life-standing-white-background_176420-34722.jpg',
-              titolo: 'Esperienza fantastica!',
-              contenuto: 'Ho apprezzato molto il servizio e la qualità del prodotto. Sicuramente tornerò!',
-              autoreNome: 'Laura',
-              autoreCognome: 'Bianchi'
-            },
-            {
-              immagine: 'https://img.freepik.com/free-photo/portrait-young-handsome-man-jean-shirt-smiling-with-crossed-arms_176420-12083.jpg',
-              titolo: 'Prodotto eccezionale!',
-              contenuto: 'Il prodotto supera le aspettative, funziona benissimo e ha un ottimo design.',
-              autoreNome: 'Marco',
-              autoreCognome: 'Rossi'
-            },
-            {
-              immagine: 'https://img.freepik.com/free-photo/mand-holding-cup_1258-340.jpg',
-              titolo: 'Servizio impeccabile!',
-              contenuto: 'Il personale è cordiale e attento alle esigenze del cliente. Consigliato!',
-              autoreNome: 'Giovanni',
-              autoreCognome: 'Verdi'
-            }
-          ],
-        };
-      },
-    });
-
-    app.mount('#app');
+app.mount('#app');
